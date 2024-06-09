fb39918d4fc2056e5298bf6d09de5787a16ce6b8
Alex <alex@arekisannda.dev>
2024-06-09 13:30:03 -0700

    Add Keebio Iris CE + vial keymaps

---
+++ keyboards/keebio/iris_ce/keymaps/vial/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// #define USE_I2C
+#define EE_HANDS
+
+// Added by Brian C. Arnold to support Vial.
+#define VIAL_KEYBOARD_UID {0xBC, 0x99, 0xF2, 0x61, 0x45, 0x7C, 0xC8, 0xB9}
+#define VIAL_UNLOCK_COMBO_ROWS {0,9}
+#define VIAL_UNLOCK_COMBO_COLS {0,5}
+
+#define VIAL_KEY_OVERRIDE_ENTRIES 32
