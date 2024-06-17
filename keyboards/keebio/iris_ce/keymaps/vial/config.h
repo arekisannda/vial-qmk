95868b64ee0e0b5f9594bf1f2df59ea058e161e5
Alex <alex@arekisannda.dev>
2024-06-17 07:09:39 -0700

    iris_ce: add 5th layer

--- keyboards/keebio/iris_ce/keymaps/vial/config.h
+++ keyboards/keebio/iris_ce/keymaps/vial/config.h
@@ -3,6 +3,8 @@
 // #define USE_I2C
 #define EE_HANDS
 
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+
 // Added by Brian C. Arnold to support Vial.
 #define VIAL_KEYBOARD_UID {0xBC, 0x99, 0xF2, 0x61, 0x45, 0x7C, 0xC8, 0xB9}
 #define VIAL_UNLOCK_COMBO_ROWS {0,9}
